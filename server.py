a5ce2f9216905b88d2ae51f5b136047dc9f66fe0
usernameisrobert <120859934+usernameisrobert@users.noreply.github.com>
2026-02-19 20:36:56 -0600

    Update server.py

--- server.py
+++ server.py
@@ -8,6 +8,17 @@ def serve_index():
     # Serves wos.html from the same directory as this script
     return send_from_directory(os.getcwd(), 'wos.html')
 
+@app.route('/programs')
+def serve_index():
+    # Serves wos.html from the same directory as this script
+    return send_from_directory(os.getcwd(), 'programs.txt')
+
+@app.route('/desktop.wasm')
+def serve_index():
+    # Serves wos.html from the same directory as this script
+    return send_from_directory(os.getcwd(), 'desktop.wasm')
+
+
 if __name__ == '__main__':
     # '0.0.0.0' makes it accessible on your local network
     app.run(host='0.0.0.0', port=5000, debug=True)
